91cb1e21eaa84af1ebf1201aa2fc8967a6af0d1e
Tristan Norton <tristan.m.norton@gmail.com>
2018-06-19 22:31:47 -0400

    Set a much better time stagger than what's built in to library

--- site.js
+++ site.js
@@ -30,25 +30,26 @@ circleOne
 		easing: 'linear'
 	});
 
-circleTwo
-	.add({
-		targets: '.circle-two',
-		translateY: triangleStop,
-		duration: 500,
-		easing: 'linear',
-		delay: 860
-	})
-	.add({
-		targets: '.circle-two',
-		translateY: triangleStop + 300,
-		duration: 500,
-		translateX: -175,
-		easing: 'linear'
-	})
-	.add({
-		targets: '.circle-two',
-		translateY: (triangleStop * 2) + 300,
-		duration: 500,
-		translateX: -175,
-		easing: 'linear'
-	});
+window.setTimeout(function(){
+	circleTwo
+		.add({
+			targets: '.circle-two',
+			translateY: triangleStop,
+			duration: 500,
+			easing: 'linear'
+		})
+		.add({
+			targets: '.circle-two',
+			translateY: triangleStop + 300,
+			duration: 500,
+			translateX: -175,
+			easing: 'linear'
+		})
+		.add({
+			targets: '.circle-two',
+			translateY: (triangleStop * 2) + 300,
+			duration: 500,
+			translateX: -175,
+			easing: 'linear'
+		});
+	}, 1000);
