1a74fbed7d29b51e5a88acc5bb79e0d2e40ac85b
Tristan Norton <tristan.m.norton@gmail.com>
2018-06-19 22:24:15 -0400

    First commit

---
+++ site.js
@@ -0,0 +1,54 @@
+var circleOne = anime.timeline({
+	loop: true
+});
+var circleTwo = anime.timeline({
+	loop: true
+});
+
+var triangleStop = (window.innerHeight / 2) - 95;
+
+
+circleOne
+	.add({
+		targets: '.circle-one',
+		translateY: triangleStop,
+		duration: 500,
+		easing: 'linear'
+	})
+	.add({
+		targets: '.circle-one',
+		translateY: triangleStop + 300,
+		duration: 500,
+		translateX: 175,
+		easing: 'linear'
+	})
+	.add({
+		targets: '.circle-one',
+		translateY: (triangleStop * 2) + 300,
+		duration: 500,
+		translateX: 175,
+		easing: 'linear'
+	});
+
+circleTwo
+	.add({
+		targets: '.circle-two',
+		translateY: triangleStop,
+		duration: 500,
+		easing: 'linear',
+		delay: 860
+	})
+	.add({
+		targets: '.circle-two',
+		translateY: triangleStop + 300,
+		duration: 500,
+		translateX: -175,
+		easing: 'linear'
+	})
+	.add({
+		targets: '.circle-two',
+		translateY: (triangleStop * 2) + 300,
+		duration: 500,
+		translateX: -175,
+		easing: 'linear'
+	});
